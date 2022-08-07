7cb5037ad949c4655b9ca62f143f89f28ac4a1f4
Dmitry Marenich <dimarenich@gmail.com>
2022-08-07 20:32:54 +0200

    Renamed and modified 'check_for_row_in_list_table'

--- superlists/functional_test/test.py
+++ superlists/functional_test/test.py
@@ -2,7 +2,10 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -15,11 +18,19 @@ class NewVisitorTest(LiveServerTestCase):
 		self.browser.quit()
 
 
-	def check_for_row_in_list_table(self, row_text):
-		'''validating a row in a list table'''
-		table = self.browser.find_element(By.ID, 'id_list_table')
-		rows = table.find_elements(By.TAG_NAME, 'tr')
-		self.assertIn(row_text, [row_text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		'''waiting a row in a list table'''
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element(By.ID, 'id_list_table')
+				rows = table.find_elements(By.TAG_NAME, 'tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 
 
 	def test_can_start_a_list_and_retrive_it_later(self):
@@ -48,9 +59,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -58,11 +67,11 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox = self.browser.find_element(By.ID, 'id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a flys')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
+		
 
         # The page updates again, and now shows both items on her list
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a flys')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
